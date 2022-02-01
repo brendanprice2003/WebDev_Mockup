7eda0b97c0dbf57a3835eb53db137b1ffda94ccc
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-01 12:30:12 +0000

    init

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style href="script.js"></style>
+</head>
+
+<body>
+	<div id="mainContainer">
+
+		<div id="titleContainer">
+			<div id="title">Web Development // &nbsp&nbsp</div>
+			<div id="subTitle">Assignment 2</div>
+		</div>
+
+		<div id="grid">
+
+			<!-- row one -->
+			<div id="row1">
+
+				<div id="itemOne">
+					<div class="titleContent">
+						Title One	
+					</div>
+				</div>
+				<div id="itemTwo">
+					<div class="titleContent">
+						Title Two
+					</div>
+				</div>
+
+			</div>
+
+			<!-- row two -->
+			<div id="row2">
+
+				<div id="itemThree">
+					<div class="titleContent">
+						Title Three
+					</div>
+				</div>
+				<div id="itemFour">
+					<div class="titleContent">
+						Title Four
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</body>
