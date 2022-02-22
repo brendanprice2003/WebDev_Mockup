1335c572260d59eaf633b74782f2210f0ee875a0
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-02-22 11:28:07 +0000

    dev

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const log = console.log.bind(console);
 const ToGit = () => {
 	window.location.href = "https://github.com/brendanprice2003/Ass.1WebDev";
 };
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	document.getElementById('create').addEventListener('click', () => {
+		for (let i=0; i<10; i++) {
+			let newElement = document.createElement(`entry${i}`);
+			document.getElementById('container').appendChild(newElement);
+			newElement.style.color = 'white';
+			newElement.innerHTML = `loop number: ${i+1}<br>`;
+			newElement.addEventListener('mouseover', () => {
+				log(newElement.innerHTML);
+			});
+		};
+	});
+});
