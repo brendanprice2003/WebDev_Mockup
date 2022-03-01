9f36aad55ad47cc228abe382c1639f7163275524
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-03-01 07:37:22 +0000

    dev

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,27 @@
 const log = console.log.bind(console);
 
-const ToGit = () => {
-	window.location.href = "https://github.com/brendanprice2003/WebDev_Mockup";
-};
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-	getWordCount();
-	// document.getElementById('create').addEventListener('click', () => {
-	// 	for (let i=0; i<10; i++) {
-	// 		let newElement = document.createElement(`entry${i}`);
-	// 		document.getElementById('container').appendChild(newElement);
-	// 		newElement.style.color = 'white';
-	// 		newElement.innerHTML = `loop number: ${i+1}<br>`;
-	// 		newElement.addEventListener('mouseover', () => {
-	// 			log(newElement.innerHTML);
-	// 		});
-	// 	};
-	// });
+	getWordCount(document.getElementById('subTitleContent'), document.getElementById('fileStatWC'));
 });
 
-const navUser = (index) => {
+// @document.element {body}, @document.element {countElement}
+var getWordCount = (body, countElement) => {
+	// Get word count of div that contains assignment content
+	if (body) {
+		wordsArr = body.innerHTML.trim().split(' ');
+		countElement.innerHTML = `Word Count: ${wordsArr.length}`;
+	};
+};
 
+var navUser = (index) => {
 	// Navigate user to passed page
 	window.location.href = `../views/sub${index}.html`;
 };
-
-const takeUserBack = () => {
-
-	// Navigate user back a page
+var takeUserBack = () => {
+	// Navigate user back to home page page
 	window.location.href = `../views/index.html`;
 };
-
-const getWordCount = () => {
-
-	// Get word count of div that contains assignment content
-	if (document.getElementById('subTitleContent')) {
-		wordsArr = document.getElementById('subTitleContent').innerHTML.trim().split(' ');
-		document.getElementById('fileStatWC').innerHTML = `Word Count: ${wordsArr.length}`;
-	};
+var ToGit = () => {
+	// Navigate user to GitHub respective repository
+	window.location.href = "https://github.com/brendanprice2003/WebDev_Mockup";
 };
