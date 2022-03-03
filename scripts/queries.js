3c925b48c09ce3a0a61ee371ad33441e6f707d8e
Brendan Price <56489848+brendanprice2003@users.noreply.github.com>
2022-03-03 22:45:18 +0000

    responsive queries

---
+++ scripts/queries.js
@@ -0,0 +1,17 @@
+
+
+// Queries from highest to smallest screen sizes, going from top to bottom of this script
+
+
+
+// const reportWindowSize = (med) => {
+
+//     med.currentTarget.innerWidth===1075 ? log(true):log(false);
+//     switch (med.currentTarget.innerWidth) {
+//         case 1075:
+//             log(1075);
+//             break;
+//     };
+// };
+
+// window.addEventListener('resize', reportWindowSize);
